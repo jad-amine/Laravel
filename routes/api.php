f3186f3e8145a83668bf9a2ca5dcf013458540e7
jad amine <jade1992amine@gmail.com>
2022-06-06 17:55:16 +0300

    Added date difference Api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function () {
+    //return view('welcome');
+    $date2 = new DateTime("1732-04-14"); 
+    $date1 = new DateTime();
+    // $diff = date_diff($date1,$date2);
+    $interval = $date1->diff($date2);
+    // echo json_encode($interval);
+    echo "difference " . $interval->days . " days ";
+});
